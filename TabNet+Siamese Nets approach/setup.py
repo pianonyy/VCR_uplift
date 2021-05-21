fb2a93f9fda0aacddcbab970aaac7bc126d832ad
pianonyy <48867176+pianonyy@users.noreply.github.com>
2021-05-21 10:55:31 +0300

    add TabNet+Siamese nets lib

---
+++ TabNet+Siamese Nets approach/setup.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from setuptools import setup
+
+
+
+setup(
+    name='tabnet',
+    version='0.1.6',
+    description='Tool .',
+
+    license='MIT License',
+    url='https://github.com/dreamquark-ai/tabnet.git'
+    )
